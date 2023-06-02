6534451443f061c00a6002ef438f182f37155e16
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-06-02 00:54:17 -0300

    Add article model

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  #makes title mandatory for an Article to be saved
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
